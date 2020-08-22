53a21243c6030efbba1b700069762090ae8bd74d
Renny Galindez <rfgalindez@gmail.com>
2020-08-21 19:25:04 -0500

    Create TheTodoHolder component with basics styles

---
+++ src/components/TheDoHolder.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="todoHolder"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+div #todoHolder {
+  min-width: 50%;
+  border-radius: 1%;
+  color: #fff;
+  height: auto;
+  padding: 3rem;
+  margin: auto;
+  background: linear-gradient(
+    90deg,
+    rgba(246, 70, 76, 1) 50%,
+    rgba(248, 116, 85, 1) 100%
+  );
+  box-shadow: 0 0 4rem 1px rgba(0, 0, 0, 0.2);
+}
+</style>
