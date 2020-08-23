7b65ab933e4617ed572529df4ebf6e58dfa4fb4f
Renny Galindez <rfgalindez@gmail.com>
2020-08-23 00:52:19 -0500

    Font import from google fonts

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;1,200;1,300&display=swap');
 body {
   background: linear-gradient(
     90deg,
