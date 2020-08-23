9e2287af48a126604c16570c4e7bd1d8aef89a61
Renny Galindez <rfgalindez@gmail.com>
2020-08-23 00:49:25 -0500

    Change icons to be used

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faSquare, faCheckSquare } from '@fortawesome/free-regular-svg-icons';
+import { faSquare, faCheckSquare, faMinusSquare } from '@fortawesome/free-regular-svg-icons';
 
-library.add([faSquare, faCheckSquare]);
+library.add([faSquare, faCheckSquare, faMinusSquare]);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
