184d88a6113e537419e0b05b1cd6534a9101cf2d
Renny Galindez <rfgalindez@gmail.com>
2020-08-24 15:38:02 -0500

    Add plus-square icon

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faSquare, faCheckSquare, faMinusSquare, faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+import {
+  faSquare,
+  faCheckSquare,
+  faMinusSquare,
+  faTrashAlt,
+  faPlusSquare,
+} from '@fortawesome/free-regular-svg-icons';
 
-library.add([faSquare, faCheckSquare, faMinusSquare, faTrashAlt]);
+library.add([faSquare, faCheckSquare, faMinusSquare, faTrashAlt, faPlusSquare]);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
