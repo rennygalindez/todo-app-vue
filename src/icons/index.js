2523990025013f692a937e225c04d02aa1a63bd2
Renny Galindez <rfgalindez@gmail.com>
2020-08-23 22:47:22 -0500

    import trash icon

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faSquare, faCheckSquare, faMinusSquare } from '@fortawesome/free-regular-svg-icons';
+import { faSquare, faCheckSquare, faMinusSquare, faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 
-library.add([faSquare, faCheckSquare, faMinusSquare]);
+library.add([faSquare, faCheckSquare, faMinusSquare, faTrashAlt]);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
