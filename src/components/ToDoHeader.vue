081468ee1d3136b4bb43b9e3bedc36bd522310d4
Renny Galindez <rfgalindez@gmail.com>
2020-08-23 23:23:28 -0500

    Create todoHeader component to show a todos count.

---
+++ src/components/ToDoHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="container-header">
+    <p id="title">Still to do</p>
+    <p id="count">{{todoPedding}}</p>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  computed: {
+    todoPedding() {
+      return this.$store.state.todos.all.filter(todo => todo.done === false)
+        .length;
+    }
+  }
+};
+</script>
+
+
+
+<style scoped>
+p {
+  font-weight: bold;
+  font-family: Poppins;
+  font-size: 2rem;
+}
+#title {
+  float: left;
+}
+#count {
+  float: right;
+}
+
+#container-header {
+  overflow: auto;
+}
+</style>
