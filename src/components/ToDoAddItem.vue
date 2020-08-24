0ef19bc682c45d76ad64a3be9bf9a6c5f2cf2b1b
Renny Galindez <rfgalindez@gmail.com>
2020-08-24 15:35:11 -0500

    CreateToDoAddItem

---
+++ src/components/ToDoAddItem.vue
@@ -0,0 +1,24 @@
+<template >
+  <div>
+    <div id="add-container">
+      <to-do-add-item-modal />
+    </div>
+  </div>
+</template>
+
+
+<script>
+import ToDoAddItemModal from "@/components/ToDoAddItemModal";
+export default {
+  components: {
+    ToDoAddItemModal
+  }
+};
+</script>
+
+
+<style>
+#add-container {
+  padding-top: 1rem;
+}
+</style>
