83a9c0cf6f652bead9445250b55855775fdc6482
Renny Galindez <rfgalindez@gmail.com>
2020-08-23 22:45:37 -0500

    Change icon aling from flex to float with padding.

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -2,35 +2,42 @@
   <div>
     <ul>
       <li v-for="(todo, index) of todos" :key="index">
-        <p id="description" :class="[{ done: todo.done }, 'vertical-align']">
-          {{ todo.name }}
+        <p id="description" :class="[{ done: todo.done }]">
+          {{
+          todo.name.length > 20 ? `${todo.name.slice(0, 20)} ...` : todo.name
+          }}
         </p>
-        <transition name="fade-translate">
-          <span class="great vertical-align" v-show="todo.done">Great</span>
-        </transition>
+        <font-awesome-icon :icon="['far','trash-alt']" size="2x" :class="['icon', 'trash-icon']" />
         <font-awesome-icon
           @click="makeDidIt(index)"
           :icon="['far', todo.done ? 'check-square' : 'square']"
           size="2x"
-          :class="['order']"
+          :class="['icon']"
         />
+        <transition name="fade-translate">
+          <p class="great" v-show="todo.done">Great</p>
+        </transition>
       </li>
     </ul>
   </div>
 </template>
-
 <script>
 export default {
   computed: {
     todos() {
       return this.$store.state.todos.all;
     },
+    shortDescription() {
+      return this.$store.state.todos.all.map(
+        todo => `${todo.name.slice(0, 20)}...`
+      );
+    }
   },
   methods: {
     makeDidIt(todoIndex) {
-      this.$store.dispatch('todos/didIt', todoIndex);
-    },
-  },
+      this.$store.dispatch("todos/didIt", todoIndex);
+    }
+  }
 };
 </script>
 
@@ -39,27 +46,33 @@ ul {
   list-style: none;
 }
 li {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border-top: 0.1rem rgba(255, 255, 255, 0.2) solid;
+  position: relative;
   font-weight: bold;
   font-family: Poppins;
+  border-top: 0.1rem rgba(255, 255, 255, 0.2) solid;
   height: 5rem;
+  padding: 1rem 0;
+  overflow: auto;
 }
 
 #description {
-  padding-left: 1rem;
-  width: 70%;
+  position: relative;
+  float: left;
+  width: 60%;
   max-height: 4rem;
   font-size: 1rem;
-  overflow-x: hidden;
-  overflow-x: visible;
-
+  padding: 1rem 0;
 }
 
-#check {
-  padding-right: 1rem;
+.great {
+  float: left;
+  margin: 1rem 0;
+  display: block;
+}
+
+.icon {
+  float: right;
+  margin: 0.5rem;
 }
 .done {
   color: rgba(255, 255, 255, 0.5);
