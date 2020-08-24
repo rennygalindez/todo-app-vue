e929a450e23081ca64e92c8e7a600714fa298245
Renny Galindez <rfgalindez@gmail.com>
2020-08-24 15:36:50 -0500

    Registration and use ToDoAddItem component

--- src/components/TheDoHolder.vue
+++ src/components/TheDoHolder.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="todoHolder">
+    <to-do-add-item />
     <to-do-header />
     <to-do-item />
   </div>
@@ -8,18 +9,21 @@
 <script>
 import ToDoItem from "@/components/ToDoItem";
 import ToDoHeader from "@/components/ToDoHeader";
+import ToDoAddItem from "@/components/ToDoAddItem";
 export default {
-  components: { ToDoItem, ToDoHeader }
+  components: { ToDoItem, ToDoHeader, ToDoAddItem }
 };
 </script>
 
 <style>
 div #todoHolder {
-  min-width: 100%;
+  position: relative;
+  width: 100%;
   border-radius: 1%;
   color: #fff;
   height: auto;
   padding: 0.5rem;
+  min-width: 330px;
 
   background: linear-gradient(
     90deg,
@@ -27,5 +31,12 @@ div #todoHolder {
     rgba(248, 116, 85, 1) 100%
   );
   box-shadow: 0 0 4rem 1px rgba(0, 0, 0, 0.2);
+}
+@media (min-width: 640px) {
+  div #todoHolder {
+    margin: auto;
+    padding: 1rem;
+    max-width: 480px;
+  }
 }
 </style>
